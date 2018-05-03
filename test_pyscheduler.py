c6ceb446953e855eb8b13a912ca2db7ac2413104
duchoa117 <duchoapc99@gmail.com>
2018-05-03 21:47:01 +0700

    .

---
+++ test_pyscheduler.py
@@ -0,0 +1,15 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from gmail import GMail, Message
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', seconds=10)
+def timed_job():
+    gmail = GMail(username="20166635@student.hust.edu.vn",password="quy.dc20166635")
+    msg = Message("hello Hoa", to="duchoapc99@gmail.com", text = "Hello world")
+    gmail.send(msg)
+    print("Sent")
+    # print('This job is run every three minutes.')
+
+
+sched.start()
